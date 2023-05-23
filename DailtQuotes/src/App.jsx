ec324104dfd496b49d32c2b1e18dc35c4aa466e5
wardhere1 <wardhereadan@gmail.com>
2023-05-23 17:43:03 +0100

    created to api calls for the app componenet

--- DailtQuotes/src/App.jsx
+++ DailtQuotes/src/App.jsx
@@ -1,10 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-  return <div>
+  const [backgroundImage, setBacgroundImage] = useState([]);
+  const apiKey = "_n7JT400hcnqytDWaOTEi6dJ0BD_PTQyKDiZOJ-fYN8";
+  const urlBackground = "https://api.unsplash.com/photos/random";
+  const urlQuotes = "https://zenquotes.io/api/quotes/";
 
-    <h1>Hello</h1>
-  </div>;
+  useEffect(() => {
+    async function quotes() {
+      const response = await fetch(urlQuotes, {
+        method: "GET",
+        // mode: "no-cors",
+      });
+      console.log(response);
+      const data = await response.json();
+      console.log(data);
+      return data;
+    }
+    quotes();
+  }, []);
+  useEffect(() => {
+    async function unsplashImg() {
+      const response = await fetch(urlBackground, {
+        headers: { Authorization: `Client-ID ${apiKey}` },
+      });
+      const data = await response.json();
+      console.log(data.urls.full);
+      return data.urls.full;
+    }
+    unsplashImg();
+  }, []);
+
+  return (
+    <div>
+      <h1>Hello</h1>
+    </div>
+  );
 }
 
 export default App;
