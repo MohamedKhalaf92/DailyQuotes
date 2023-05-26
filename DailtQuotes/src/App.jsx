3e716e4e26148d675d0894cb8a22dbe96ab67f34
Mohamed Khalaf <mohamed.khalaf@infinityworks.com>
2023-05-26 16:51:30 +0100

    advice-containter

--- DailtQuotes/src/App.jsx
+++ DailtQuotes/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState, useEffect } from "react";
 import MainContent from "./components/MainContent";
+import Header from "./components/Header";
 
 function App() {
   const [backgroundImage, setBackgroundImage] = useState("");
   const [next, setNext] = useState(0);
   const [advice, setAdvice] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   const apiKey = "_n7JT400hcnqytDWaOTEi6dJ0BD_PTQyKDiZOJ-fYN8";
   const urlBackground = "https://api.unsplash.com/photos/random";
   const urlQuotes = "https://api.adviceslip.com/advice";
@@ -25,8 +27,19 @@ function App() {
   }, [next]);
 
   const newAdvice = () => {
-    setNext((prev) => prev + 1);
+    setIsLoading(true);
+    setTimeout(() => {
+      setNext((prev) => prev + 1);
+      setIsLoading(false);
+    }, 3000);
+
   };
+  // const newAdvice = async () => {
+  //   await setIsLoading(true);
+
+  //  await setNext((prev) => prev + 1);
+  //   setIsLoading(false);
+  // };
   //How to add authorisation api key to header when required.
   // useEffect(() => {
   //   async function unsplashImg() {
@@ -42,16 +55,19 @@ function App() {
   //   unsplashImg();
   // }, []);
 
-  //Create next button to reset
-
-  //try reize box when text is long
-
   //title above th adviceContainer.
 
   return (
+    <>
+    <></>
     <div className="advice-container">
-      <MainContent advice={advice} newAdvice={newAdvice} />
+      <MainContent
+        advice={advice}
+        newAdvice={newAdvice}
+        isLoading={isLoading}
+      />
     </div>
+    </>
   );
 }
 
--- DailtQuotes/src/components/MainContent.jsx
+++ DailtQuotes/src/components/MainContent.jsx
@@ -1,12 +1,22 @@
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
+import LoadingIcons from "react-loading-icons";
 export default function MainContent(props) {
   console.log(props);
   return (
-    <div>
+    
+    <div className="inner-containter">
       <h1 className="title">Advice Of The Day is Here!</h1>
       <h2 className="sub-title">Note it Down ✍️</h2>
-      <p className="advice">{props.advice}</p>
-      <button className="button" onClick={props.newAdvice}>
-        {props.isLoading ? "loading" : "next"}
+      <p className="advice">
+        {" "}
+        {props.isLoading ? <LoadingIcons.Rings /> : props.advice}
+      </p>
+      <button
+        className={`button ${props.isLoading ? "button-loading" : ""}`}
+        disabled={props.isLoading}
+        onClick={props.newAdvice}
+      >
+        {props.isLoading ? "loading..." : "Next"}
       </button>
     </div>
   );
